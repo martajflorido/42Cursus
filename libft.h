96134cbe702147ecb5a3daeea82327bdf85a1820
martjim2 <martajflorido@gmail.com>
2023-09-19 13:09:17 +0200

    libft.h

---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martjim2 <martjim2@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 15:25:27 by martjim2          #+#    #+#             */
+/*   Updated: 2023/09/19 12:35:29 by martjim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBTF_H
+#define LIBFT_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_isalpha(int c);
+int	ft_isdigit(int c);
+
+
+
+#endif
